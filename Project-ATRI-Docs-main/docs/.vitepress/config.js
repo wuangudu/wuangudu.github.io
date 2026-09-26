afbff2b67cbb766647072fb665b26b04b1b687ca
wuangudu <a112233yi@qq.com>
2026-09-26 20:22:29 +0800

    Update config.js

--- Project-ATRI-Docs-main/docs/.vitepress/config.js
+++ Project-ATRI-Docs-main/docs/.vitepress/config.js
@@ -18,7 +18,6 @@ export default {
             { text: '常见问题', link: '/faq' },
             { text: '加入 QQ 群', link: 'https://qun.qq.com/universal-share/share?ac=1&authKey=q5cDheD61bsd59f3gWVJ36OnqYJXFouTwgB9tUIkj0eBJw5mOpviDoGxNVtoSlmX&busi_data=eyJncm91cENvZGUiOiIxMTA0MTQ4NjQ1IiwidG9rZW4iOiJSMWhwTEJuREJuOTI1QnZGSjgwMVpwYWpGMHJ1bTkwajhBbGdhdjRpdHp6bGsya2pudGdVc1Bjc2trUDkxMXlyIiwidWluIjoiMTg1MDYzMDY3MCJ9&data=M-qT138VfoVy5SKnO77nicW3PIXOnS7VnxxhddDoehfb_m4qpGzxJ79FNV5hTzjVeA3BmHVSlbxR7GFJB4gfoQ&svctype=4&tempid=h5_group_info', target: '_blank' },
         ],
-        // sidebar 按需保留或删除
         socialLinks: [
             { icon: 'github', link: 'https://github.com/wuangudu/wuangudu.github.io' }
         ],
